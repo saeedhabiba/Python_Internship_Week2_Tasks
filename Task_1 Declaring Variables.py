a1f87463b976b90edee42c2bb13eade7a6cb3c11
Umme Habiba <saeedhabiba001@gmail.com>
2025-06-28 19:23:15 +0500

    Add files via upload

---
+++ Task_1 Declaring Variables.py
@@ -0,0 +1,17 @@
+#Declaring variables
+#mini profile for fictional user
+#i want to be fictional character for my code
+name = "habiba"
+age = 21
+current_year = 2025
+hobby = "coding"
+country = "Pakistan"
+expected_graduation_year = current_year + 4
+years_left = expected_graduation_year - current_year
+
+print("i am", name)
+print("i am ", age ,"years old")
+print("i live in", country)
+print("i like", hobby, "in my free time")
+print("inshAllah my degree is expected to be completed in", expected_graduation_year , "years")
+print("years left to my degree", years_left)
